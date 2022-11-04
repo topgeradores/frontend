24430e087cff9717ea2831da6dc2b0701c2dfb16
topgeradores <96098138+topgeradores@users.noreply.github.com>
2022-11-04 13:03:24 -0300

    Alteração de levantamento para dimensionamento

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -24,7 +24,7 @@ export const Home = () => {
       <div className="row">
         <Subtitle
           color="black"
-          text="- Levantamento de cargas"
+          text="- Dimensionamento de carga"
         />
       </div>
       <div className="row">
